7aecd87912d70d0c76bad0284d8bf79a0ed8ce81
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2024-10-10 16:45:04 +0100

    up

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -74,3 +74,5 @@ export const refreshUser = createAsyncThunk(
 		}
 	}
 );
+
+
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // <https://vitejs.dev/config/>
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     sourcemap: true,
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// <https://vitejs.dev/config/>
 export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
   },
+  // Dodaj to, aby Vite obsługiwało poprawnie historie przeglądarki
+  server: {
+    historyApiFallback: true,
+  }
 });
