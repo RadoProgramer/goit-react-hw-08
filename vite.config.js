d965cf8701c87b02bba192835abbbd7d646fd5d5
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2024-10-10 17:39:13 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,24 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // <https://vitejs.dev/config/>
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     sourcemap: true,
-//   },
-// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// <https://vitejs.dev/config/>
 export default defineConfig({
-	plugins: [react()],
-	build: {
-		sourcemap: true,
-	},
-	server: {
-		historyApiFallback: true,
-	},
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
 });
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+// 	plugins: [react()],
+// 	build: {
+// 		sourcemap: true,
+// 	},
+// 	server: {
+// 		historyApiFallback: true,
+// 	},
+// });
